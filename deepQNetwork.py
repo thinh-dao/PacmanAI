084399948361ddcab5a10b2fc0d52be29bdb36f8
Thinh Dao <ducthinh19102003@gmail.com>
2023-06-11 12:16:03 +0000

    Update deepQNetwork

--- deepQNetwork.py
+++ deepQNetwork.py
@@ -7,27 +7,13 @@ class DeepQNetwork():
     A model that uses a Deep Q-value Network (DQN) to approximate Q(s,a) as part
     of reinforcement learning.
     """
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, model):
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         print("Running on ", self.device)
-        self.num_actions = action_dim
-        self.state_size = state_dim
-
+        self.model = model.to(self.device)
         self.learning_rate = 0.2
         self.numTrainingGames = 2000
         self.batch_size = 64
-        self.cnt = 0
-
-        self.model = nn.Sequential(
-            nn.Linear(in_features=self.state_size, out_features=512, bias=True),
-            nn.ReLU(),
-            nn.Linear(in_features=512, out_features=512, bias=True),
-            nn.ReLU(),
-            nn.Linear(in_features=512, out_features=256, bias=True),
-            nn.ReLU(),  
-            nn.Linear(in_features=256, out_features=self.num_actions, bias=True)
-        ).to(self.device)
-
         self.loss_fn = nn.MSELoss().to(self.device)
         self.optimizer = torch.optim.Adagrad(params=self.model.parameters(), lr=0.05)
 
@@ -59,3 +45,33 @@ class DeepQNetwork():
         self.optimizer.step()
         torch.nn.utils.clip_grad_norm_(self.model.parameters(), 100)
         self.cnt += 1
+
+class MLP(DeepQNetwork):
+    def __init__(self, state_dim, action_dim):
+        self.model = nn.Sequential(
+            nn.Linear(in_features=state_dim, out_features=512, bias=True),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=512, bias=True),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=256, bias=True),
+            nn.ReLU(),  
+            nn.Linear(in_features=256, out_features=action_dim, bias=True)
+        )
+        super().__init__(self.model)
+
+class CNN(DeepQNetwork):
+    def __init__(self, state_dim, action_dim, state_history):
+        img_height, img_width, n_channels = state_dim
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels=n_channels * state_history, out_channels=32, kernel_size=8, stride=4, padding=((4 - 1) * img_height - 4 + 8) // 2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32,out_channels=64, kernel_size=4, stride=2, padding=((2 - 1) * img_height - 2 + 4) // 2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64,out_channels=64, kernel_size=3, stride=1, padding=((1 - 1) * img_height - 1 + 3) // 2),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(in_features=64 * img_height * img_width, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=action_dim)
+        )
+        super().__init__(self.model)
